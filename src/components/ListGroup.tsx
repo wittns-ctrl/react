963a6704e18d821226ddf3919867bbb5e4e63f24
wittns <mugishawitness15@gmail.com>
2026-04-21 20:50:17 +0200

    conditional rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,22 @@
 function ListGroup() {
+
+    let items = [
+        'New york',
+        'san francisco',
+        'tokyo',
+        'london',
+        'paris'
+    ]
+items = []
+
   return (
+    <>
+    <h1>List</h1>
+    {items.length === 0 && <p>Not found</p>}
     <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
+    {items.map(item => <li key={item}>{item}</li>)}
     </ul>
+    </>
   );
 }
 
