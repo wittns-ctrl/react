8279708a3e1cef4cdaf2e1d5bd8ae8fcb58a446c
wittns <mugishawitness15@gmail.com>
2026-04-21 20:21:46 +0200

    list group components

---
+++ src/Message.tsx
@@ -0,0 +1,13 @@
+
+
+ // PascalCasing
+
+function Message(){ 
+     // JSX: javascript XML
+     const name = "witness"
+     if(name)
+    return <h1>Hello {name}</h1>
+    return <h1>hello world</h1>
+}
+ 
+export default Message;
